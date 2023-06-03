119bd8c072c432d6a19dcce2b88fa77fa8d175a5
Wojciech Weremczuk <wojciech.weremczuk5@gmail.com>
2023-06-03 12:46:25 +0200

    Displaying products in the frontend.

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Routes, Route } from 'react-router-dom';
+import React from "react";
+import Home from "./components/Home";
+import Product from "./components/Product";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
     </div>
+  );
+}
+
+function Main() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:id" element={<Product />} />
+    </Routes>
   );
 }
 
